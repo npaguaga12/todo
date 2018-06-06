5547a38f9322de10759304182a05df13bcd19faa
Natalia Paguaga <npaguaga12@gmail.com>
2018-06-06 19:39:47 -0400

    Todo lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'todo_items#index'
+
+  root 'tasks#index'
   resources :todo_items
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
